e5545453cbf1121f6082d8de98ed20c5cffb8977
Mister Magoo <mister.magoo+githubsql@gmail.com>
2020-06-15 16:06:16 +0100

    Tidy Before Name Change

---
+++ Blazor/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using BlazorTwins311.Lib;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var builder = WebApplicationHost.CreateDefaultBuilder(args);
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+        builder.Services.AddSingleton<TestCase, TestCase>();
+        builder.Services.AddHttpContextAccessor();
+        builder.Services.AddScoped<IPreRenderFlag, PreRenderFlag>();
+        var app = builder.Build();
+        app.MapRazorPages();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.MapControllers();
+        app.MapBlazorHub();
+        app.MapFallbackToPage("/Index");
+        //OpenBrowser("http://localhost:5000/");
+        await app.RunAsync();
+    }
+
+    public static void OpenBrowser(string url)
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            Process.Start("xdg-open", url);
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            Process.Start("open", url);
+        }
+        else
+        {
+            // throw 
+        }
+    }
+}
---
+++ Samples/BlazorSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+using System.Threading.Tasks;
+
+namespace BlazorSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>()
+                .Build()
+                .Run();
+        }
+
+    }
+}
